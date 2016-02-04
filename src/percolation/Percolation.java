ff4caad06b07d1505588f0b706063573747e5d2f
Sebastien Torres <baztorres@gmail.com>
2016-02-04 21:40:10 +0000

    Percolation initial

---
+++ src/percolation/Percolation.java
@@ -0,0 +1,43 @@
+package percolation;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ * Created by st on 04/02/16.
+ */
+public class Percolation {
+
+    public Percolation(int N){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+    public void open(int i, int j){
+
+    }
+
+    public boolean isOpen(){
+        boolean isOpen = false;
+
+        return isOpen;
+    }
+
+    public boolean isFull(){
+        boolean isFull = false;
+
+        return isFull;
+    }
+
+    public boolean percolates(){
+
+        boolean doesPercolate = false;
+
+        return doesPercolate;
+
+    }
+}
