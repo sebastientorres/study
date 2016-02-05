075f50df18af9d952c4aee0ad69bf88ca679f984
Sebastien Torres <baztorres@gmail.com>
2016-02-05 21:40:42 +0000

    New class - PercolationStats

---
+++ src/percolation/PercolationStats.java
@@ -0,0 +1,44 @@
+package percolation;
+
+/**
+ * Created by st on 05/02/16.
+ */
+public class PercolationStats {
+
+    public PercolationStats(int N, int T){
+
+    }
+
+    public double mean() {
+
+        double result = 0;
+
+        return result;
+
+    }
+
+    public double stddev() {
+
+        double result = 0;
+
+        return result;
+    }
+
+    public double confidenceLo() {
+
+        double result = 0;
+
+        return result;
+    }
+
+    public double confidenceHi() {
+
+        double result = 0;
+
+        return result;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
