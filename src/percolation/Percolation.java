39ee0df5de544ee5693a96faf46f199322f7836c
Sebastien Torres <baztorres@gmail.com>
2016-02-27 19:43:29 +0000

    Autograder style bussiness...

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,15 +1,7 @@
-package percolation;
+//package percolation;
 
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Stream;
 
 /**
  * Created by st on 04/02/16.
@@ -34,43 +26,52 @@ public class Percolation {
         initialiseSystem(N);
     }
 
-    private void initialiseSystem(int N){
-        int value = 1;
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                system[i][j]=0;
-                value++;
+    private void initialiseSystem(int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                system[i][j] = 0;
             }
         }
     }
+    //    @Override
+    private boolean connected(int i, int j, int k, int l) {
+        i--;
+        j--;
+        k--;
+        l--;
+        int p = (i * systemDimension) + j;
+        int q = (k * systemDimension) + l;
+        return wquf.connected(p , q);
+    }
+    public void open(int i, int j) {
 
-    public void open(int i, int j){
+        checkBoundary(i,j);
+
         // Doing [i|j]-- to address off by 1.
         i--;
         j--;
 
-        checkBoundary(i,j);
         int p = (systemDimension * i) + j;
 
-        if(!(i-1 < 0)){
-            if(isOpen(i-1,j)){
+        if (!(i-1 < 0)){
+            if (isOpen(i-1,j)) {
                 wquf.union( p, ((systemDimension * (i-1)) +j ));
             }
         }
 
-        if(!(i+1 >= systemDimension)){
-            if(isOpen(i+1, j)){
+        if (!(i+1 >= systemDimension)) {
+            if (isOpen(i+1, j)) {
                 wquf.union( p, (systemDimension * (i+1)) + j);
             }
         }
 
-        if(!(j-1 < 0)){
-            if(isOpen(i, j-1)){
+        if (!(j-1 < 0)){
+            if (isOpen(i, j-1)) {
                 wquf.union(p, (systemDimension * i) + (j-1));
             }
         }
 
-        if ( !(j+1 >= systemDimension)){
+        if ( !(j+1 >= systemDimension)) {
             if(isOpen(i, j+1)){
                 wquf.union(p, (systemDimension * i) + (j+1));
             }
@@ -80,28 +81,29 @@ public class Percolation {
 
     }
 
-    public boolean isOpen(int i, int j){
-        if(system[i][j] == 1){
-            return true;
-        }
-        else {
-            return false;
-        }
+    public boolean isOpen(int i, int j) {
+        return system[i][j] == 1;
     }
 
-    public boolean isFull(int i, int j){
-        boolean isFull = false;
+    public boolean isFull(int i, int j) {
+        i--;
+        j--;
 
-        return isFull;
+        for (int counter = 0 ; counter < systemDimension; counter ++) {
+            if (wquf.connected((systemDimension * i) + j, counter)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // i and j are the coordinates of the system as a grid (2d array), not the system as a
     // 1d array - we need to translate \ convert 2d to 1d...
     // i and j need to become p and q at the top and bottom of the system
 
-    public boolean percolates(){
-        for (int i = 0 ; i < systemDimension; i++){
-            for (int j = (numberOfSites - systemDimension)-1; j < numberOfSites ; j++){
+    public boolean percolates() {
+        for (int i = 0 ; i < systemDimension; i++) {
+            for (int j = (numberOfSites - systemDimension)-1; j < numberOfSites ; j++) {
                     if (wquf.connected(i, j)) {
                         return true;
                 }
@@ -110,11 +112,11 @@ public class Percolation {
         return false;
     }
 
-    private void checkBoundary(int i, int j){
-        if(i < 0 || j < 0){
+    private void checkBoundary(int i, int j) {
+        if (i < 0 || j < 0){
             throw new IndexOutOfBoundsException("i or j can't be less than 0");
         }
-        if(i > systemDimension || j > systemDimension){
+        if (i > systemDimension || j > systemDimension) {
             throw new IndexOutOfBoundsException("i or j can't be greater than " + systemDimension);
         }
     }
@@ -132,22 +134,22 @@ public class Percolation {
     }
 
     private void printWquf(){
-        for(int p = 0; p < numberOfSites; p++){
+        for (int p = 0; p < numberOfSites; p++) {
             System.out.print(p + "\t");
         }
         System.out.println();
-        for(int p = 0; p < numberOfSites; p++){
+        for (int p = 0; p < numberOfSites; p++) {
             System.out.print(wquf.find(p) + "\t");
         }
     }
 
-    private double getNumberOfOpenSites(){
-        if(numberOfOpenSites > 0){
+    private double getNumberOfOpenSites() {
+        if (numberOfOpenSites > 0) {
             return numberOfOpenSites;
         } else {
-            for (int i = 0 ; i < systemDimension; i++){
-                for (int j = 0 ; j < systemDimension; j++){
-                    if (system[i][j] == 1){
+            for (int i = 0 ; i < systemDimension; i++) {
+                for (int j = 0 ; j < systemDimension; j++) {
+                    if (system[i][j] == 1) {
                         numberOfOpenSites++;
                     }
                 }
@@ -156,20 +158,17 @@ public class Percolation {
         }
     }
 
-    private double getOpenSitePercentage(){
-
-
-        double result = Double.valueOf(getNumberOfOpenSites()) / Double.valueOf(numberOfSites);
-        return result;
+    private double getOpenSitePercentage() {
+        return Double.valueOf(getNumberOfOpenSites()) / Double.valueOf(numberOfSites);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int siteSize = 100;
 
         Percolation percolation = new Percolation(siteSize);
 
-        while(!percolation.percolates()){
+        while (!percolation.percolates()) {
             Random pRand = new Random();
             Random qRand = new Random();
 
