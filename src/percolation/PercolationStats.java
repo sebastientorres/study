332cccdb058e0c827fef2cdda0f0612a7cd33d74
Sebastien Torres <baztorres@gmail.com>
2016-02-27 18:14:39 +0000

    Constructor and input check

--- src/percolation/PercolationStats.java
+++ src/percolation/PercolationStats.java
@@ -7,11 +7,20 @@ public class PercolationStats {
 
     public PercolationStats(int N, int T){
 
+        if(N <= 0 || T <= 0){
+            throw new IllegalArgumentException("N and \\ or T should be 1 or more.");
+        }
+
+        for (int i = 0; i < T; i++){
+            Percolation percolation = new Percolation(N);
+        }
+
     }
 
     public double mean() {
 
         double result = 0;
+
 
         return result;
 
@@ -39,6 +48,11 @@ public class PercolationStats {
     }
 
     public static void main(String[] args){
+
+        int dimension = Integer.valueOf(args[0]);
+        int numberOfTimes = Integer.valueOf(args[1]);
+
+        PercolationStats stats = new PercolationStats(dimension, numberOfTimes);
 
     }
 }
